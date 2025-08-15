aabcc3670a94ec2d37a42b2683e9dc6a4592cccd
reisxd <29177546+reisxd@users.noreply.github.com>
2025-08-15 20:48:21 +0300

    feat: add language resources and turkish language

--- client/ui/src/components/i18nResources.js
+++ client/ui/src/components/i18nResources.js
@@ -1,8 +1,20 @@
 import EnglishLanguage from '../lang/en.json';
+import TurkishLanguage from '../lang/tr.json';
+import FrenchLanguage from '../lang/fr.json';
+import VietnameseLanguage from '../lang/vi.json';
 
 const resources = {
   en: {
     translation: EnglishLanguage
+  },
+  tr: {
+    translation: TurkishLanguage
+  },
+  fr: {
+    translation: FrenchLanguage
+  },
+  vi: {
+    translation: VietnameseLanguage
   }
 };
 
