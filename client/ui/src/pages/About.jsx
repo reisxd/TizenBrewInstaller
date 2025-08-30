1dbe31140580c7fe33acb94acb0d7d5e3bcb9579
Cooper <coop@coopeeo.xyz>
2025-08-30 14:54:46 -0500

    add some stuff

--- client/ui/src/pages/About.jsx
+++ client/ui/src/pages/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
         <div className="flex justify-center align-center">
             <div className="flex flex-col items-center">
                 <img src={TBLogo} className="h-[24vh] w-auto mt-16" />
-                <p className="text-gray-300 mt-4 text-base/7">App Version: {appVersion}</p>
+                <p className="text-gray-300 mt-4 text-base/7">App Version: {appVersion} (Installer)</p>
                 <p className="text-gray-300 mt-4 text-base/7">Tizen Version: {tizenVersion}</p>
                 <p className="text-gray-300 mt-4 text-base/7">TV Model: {tvModel}</p>
             </div>
